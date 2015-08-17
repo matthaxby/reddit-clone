3810f1d97cbd6e694aff3827354b8a64b9bd852b
Matt Haxby <matthaxby@gmail.com>
2015-08-17 11:42:40 -0600

    made a new repo for just the reddit exercise

---
+++ javascripts/controllers.js
@@ -0,0 +1,35 @@
+var app = angular.module('reddit', ['angularMoment'])
+
+
+app.controller('RedditController', function($scope) {
+  $scope.search = ''
+  $scope.posting = false
+  $scope.submitPost = function() {
+    $scope.posting = true
+  }
+  $scope.sortby = '-time'
+  $scope.posts = []
+  $scope.newPost = function(title, author, image, description) {
+    $scope.posts.push({'title': title, 'author': author, 'image': image, 'description': description, 'time': new Date(), 'likes': 0, 'comments': [], 'id': $scope.posts.length, show: false, make: false})
+    $scope.posting = false
+  }
+  $scope.upVote = function(id) {
+    $scope.posts[id].likes++
+  }
+  $scope.downVote = function(id) {
+    $scope.posts[id].likes--
+  }
+  $scope.showComments = function(id) {
+    $scope.posts[id].show = !$scope.posts[id].show
+  }
+  $scope.showMakeComment = function (id) {
+    $scope.posts[id].make = !$scope.posts[id].make
+  }
+  $scope.makeComment = function(id, name, content) {
+    $scope.posts[id].comments.push(name + ': ' + content)
+    $scope.posts[id].make = !$scope.posts[id].make
+  }
+  $scope.close = function() {
+    $scope.posting = false
+  }
+})
